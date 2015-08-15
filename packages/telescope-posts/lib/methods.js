4aa6504af4b8dcb9edc1917d7c28e3dfe89ad5ca
Derek Shanahan <derek@dshan.me>
2015-08-14 20:47:16 -0700

    trying to fix bug killing main site

--- packages/telescope-posts/lib/methods.js
+++ packages/telescope-posts/lib/methods.js
@@ -280,7 +280,7 @@ Meteor.methods({
   increasePostViews: function(postId, sessionId){
 
     check(postId, String);
-    check(sessionId, Match.Any);
+    check(sessionId, String);
 
     this.unblock();
 
