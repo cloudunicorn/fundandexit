9793890e0c0f023bbf5537857a7e3f9422d492bc
Derek Shanahan <derek@dshan.me>
2015-08-24 14:48:58 -0700

    Remove undefined template

--- packages/custom-fundexits/lib/client/custom_templates.js
+++ packages/custom-fundexits/lib/client/custom_templates.js
@@ -2,6 +2,5 @@
 Template.custom_post_title.replaces("post_title");
 Template.custom_post_item.replaces("post_item");
 Template.custom_post_content.replaces("post_content");
-Template.custom_nav.replaces("nav");
 Template.custom_newsletter_banner.replaces("newsletter_banner");
 Template.custom_day_heading.replaces("day_heading");
